597b6104d5007770c9b3a87ec20de569eb09f9b9
Domza64 <juricicdominik@protonmail.com>
2024-08-16 13:47:12 +0200

    Improve player | Implement autoasign player number

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,13 @@
 extends CharacterBody2D
 
+var playerNumber = GameManager.addPlayer()
+@export var skin : String = "steve"
+
+func _ready() -> void:
+	print("Player: " + str(playerNumber))
+	var skin = load("res://player/skin/player_skin_" + skin + ".tscn").instantiate()
+	add_child(skin)
+
 const BASE_SPEED = 300.0
 const JUMP_VELOCITY = -1200.0
 const MAX_SPEED_MODIFIER = 5.0  # Maximum speed multiplier
@@ -7,13 +15,8 @@ const SPEED_INCREMENT = 2.5  # Speed multiplier increase per second
 const SPEED_DECREMENT = 10.0  # Speed multiplier decrease per second
 const DEFAULT_SKIN = "steve"
 
-@onready var player = $".."
-@onready var playerNum = player.get_meta("playerNum")
-@onready var skin = player.get_meta("skin")
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 var speed_modifier = 1.0  # Initial speed multiplier
-
 # Mapping player numbers to input actions
 var input_map = {
 	1: {
@@ -28,15 +31,6 @@ var input_map = {
 	}
 }
 
-func _ready():
-	if playerNum == null:
-		push_error("Metadata 'playerNum' must be set for Player instance: " + self.to_string() + "!")
-	if skin == null:
-		skin = DEFAULT_SKIN
-		
-	var skin = load("res://player/player_skin_" + skin + ".tscn").instantiate()
-	add_child(skin)
-
 func _physics_process(delta):
 	handle_gravity(delta)
 	handle_movement(delta)
@@ -47,7 +41,7 @@ func handle_gravity(delta):
 		velocity.y += gravity * delta
 
 func handle_movement(delta):
-	var input_actions = input_map.get(playerNum)
+	var input_actions = input_map.get(playerNumber)
 	if input_actions:
 		var direction = Input.get_axis(input_actions["left"], input_actions["right"])
 		if direction != 0:
@@ -60,7 +54,7 @@ func handle_movement(delta):
 			velocity.x = move_toward(velocity.x, 0, BASE_SPEED * speed_modifier)
 
 func handle_jump():
-	var input_actions = input_map.get(playerNum)
+	var input_actions = input_map.get(playerNumber)
 	if input_actions and Input.is_action_just_pressed(input_actions["jump"]) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
---
+++ scripts/game_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var players = 0
+
+func addPlayer():
+	players += 1
+	return players
